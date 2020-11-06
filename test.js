d4656f7dc7d8055e71380dc47bde7a4a0f4434fa
Shubham Shekhar <shubhamshekhar123.ss@gmail.com>
2020-11-06 21:00:12 +0530

    first

---
+++ test.js
@@ -0,0 +1,18 @@
+const chai=require('chai')
+//const server = require('./index.js')
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const should = chai.should();
+
+describe('loading express', ()=> {
+    it("return list of blog", (done) => {
+        chai.request("http://localhost:4000/").get("/").end((err, res) => {
+            if(err){
+                console.log(err)
+                return
+            }
+            res.should.have.property('status');
+            done();
+        })
+    })
+})
